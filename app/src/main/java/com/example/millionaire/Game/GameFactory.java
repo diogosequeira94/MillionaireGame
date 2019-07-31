451a5515aedac294f5fe5208bb6cb4a2d181487c
Diogo <diogosequeira@hotmail.fr>
2019-07-31 11:05:49 +0100

    google login

--- app/src/main/java/com/example/millionaire/Game/GameFactory.java
+++ app/src/main/java/com/example/millionaire/Game/GameFactory.java
@@ -1,4 +1,25 @@
 package com.example.millionaire.Game;
 
+import com.example.millionaire.QuestionTypes.ChallengerQuestions;
+import com.example.millionaire.QuestionTypes.QuestionItem;
+import com.example.millionaire.QuestionTypes.Questions;
+
 public class GameFactory {
+
+
+    public static QuestionItem updateQuestion(GameTypes gameTypes) {
+
+        ChallengerQuestions challengerQuestions = new ChallengerQuestions();
+        Questions questions = new Questions();
+
+        switch (gameTypes) {
+
+            case BASIC:
+
+        }
+
+        //   return}
+        // }
+        return null;
+    }
 }
---
+++ app/src/main/java/com/example/millionaire/Utils/GoogleLogin.java
@@ -0,0 +1,16 @@
+package com.example.millionaire.Utils;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.millionaire.R;
+
+public class GoogleLogin extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_google_login);
+    }
+}
