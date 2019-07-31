bc5fda53abda174705400eb48faae288663f6b3a
Diogo <diogosequeira@hotmail.fr>
2019-07-31 18:08:45 +0100

    settings menu

--- app/src/main/java/com/example/millionaire/TriviaPages/HomePage.java
+++ app/src/main/java/com/example/millionaire/TriviaPages/HomePage.java
@@ -2,12 +2,19 @@ package com.example.millionaire.TriviaPages;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
 import android.media.AudioManager;
+import android.media.Image;
 import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.view.ContextThemeWrapper;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.PopupMenu;
 import android.widget.Toast;
 
 import com.example.millionaire.Game.GameLogic;
@@ -15,12 +22,15 @@ import com.example.millionaire.Game.GameModes;
 import com.example.millionaire.R;
 import com.example.millionaire.Utils.LoadingStartGame;
 
-public class HomePage extends AppCompatActivity {
+public class HomePage extends AppCompatActivity implements PopupMenu.OnMenuItemClickListener {
 
     private Button startButton;
     private Button practiceButton;
     private Button gameModes;
     private MediaPlayer mMediaPlayer;
+    private ImageView settings;
+    private Menu menu;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,9 +39,14 @@ public class HomePage extends AppCompatActivity {
 
         getSupportActionBar().hide();
 
+
+
         startButton = findViewById(R.id.buttonStart);
         practiceButton = findViewById(R.id.button2);
         gameModes = findViewById(R.id.gameModes);
+        settings = findViewById(R.id.settings);
+
+       // menu = findViewById(R.id.)
 
         mMediaPlayer = new MediaPlayer();
         mMediaPlayer = MediaPlayer.create(this, R.raw.theme);
@@ -68,9 +83,35 @@ public class HomePage extends AppCompatActivity {
                 finish();
             }
         });
+
     }
 
 
 
+    public void showPopup(View view){
+        Context wrapper = new ContextThemeWrapper(this, R.style.Popup);
 
+        PopupMenu popupMenu = new PopupMenu(wrapper,view);
+        popupMenu.setOnMenuItemClickListener(this);
+        popupMenu.inflate(R.menu.settings_menu);
+        popupMenu.show();
+    }
+
+    @Override
+    public boolean onMenuItemClick(MenuItem menuItem) {
+
+        switch (menuItem.getItemId()){
+            case R.id.myProfile:
+                Toast.makeText(this, "Shows Profile", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.settingsOption:
+                Toast.makeText(this, "Shows Settings", Toast.LENGTH_SHORT).show();
+                return true;
+
+             default:
+                 return false;
+        }
+
+
+    }
 }
--- app/src/main/java/com/example/millionaire/Utils/GoogleLogin.java
+++ app/src/main/java/com/example/millionaire/Utils/GoogleLogin.java
@@ -1,8 +1,10 @@
 package com.example.millionaire.Utils;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -10,18 +12,27 @@ import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
 import com.example.millionaire.R;
+import com.example.millionaire.TriviaPages.HomePage;
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.SignInButton;
 import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.common.api.Status;
+import com.google.android.gms.common.internal.SignInButtonImpl;
 
 public class GoogleLogin extends AppCompatActivity implements View.OnClickListener, GoogleApiClient.OnConnectionFailedListener {
 
     private LinearLayout profileSection;
     private Button signOut;
     private SignInButton signInButton;
-    private TextView name, email;
-    private ImageView profilePic;
+    private TextView name, email, noLogin;
+    private ImageView profilePic, logoImage;
 
     private GoogleApiClient googleApiClient;
     private static final int REQ_CODE = 9001;
@@ -31,16 +42,23 @@ public class GoogleLogin extends AppCompatActivity implements View.OnClickListen
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_google_login);
 
+        getSupportActionBar().hide();
+
 
         profileSection = (LinearLayout) findViewById(R.id.profilesection);
         signOut = findViewById(R.id.logout);
         signInButton = (SignInButton) findViewById(R.id.button_login);
         name = findViewById(R.id.name);
+        noLogin = findViewById(R.id.continueButton);
         email = findViewById(R.id.email);
         profilePic = findViewById(R.id.prof_pic);
+        logoImage = findViewById(R.id.logocenter);
+        GoogleSignInOptions signInOptions = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).requestEmail().build();
+        googleApiClient = new GoogleApiClient.Builder(this).enableAutoManage(this,this).addApi(Auth.GOOGLE_SIGN_IN_API, signInOptions).build();
 
         signInButton.setOnClickListener(this);
         signOut.setOnClickListener(this);
+        noLogin.setOnClickListener(this);
 
         //When activity starts this is invisible
         profileSection.setVisibility(View.GONE);
@@ -52,10 +70,92 @@ public class GoogleLogin extends AppCompatActivity implements View.OnClickListen
     @Override
     public void onClick(View view) {
 
+        switch (view.getId()){
+
+            case R.id.button_login:
+                signIn();
+                break;
+
+            case R.id.logout:
+                signOut();
+                break;
+
+            case R.id.continueButton:
+                startActivity( new Intent(getApplicationContext(), HomePage.class));
+                finish();
+        }
     }
 
     @Override
     public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
 
+    }
+
+    private void signIn(){
+
+        Intent intent = Auth.GoogleSignInApi.getSignInIntent(googleApiClient);
+        // When you use Activity for result you need to override OnActivityResult
+        startActivityForResult(intent,REQ_CODE);
+    }
+
+    private void signOut(){
+
+        Auth.GoogleSignInApi.signOut(googleApiClient).setResultCallback(new ResultCallback<Status>() {
+            @Override
+            public void onResult(@NonNull Status status) {
+                updateUI(false);
+            }
+        });
+
+    }
+
+    private void handleResult(GoogleSignInResult result){
+
+        if(result.isSuccess()){
+            GoogleSignInAccount account = result.getSignInAccount();
+
+            String name2 = account.getDisplayName();
+            String email2 = account.getEmail();
+            String image_url = account.getPhotoUrl().toString();
+
+            name.setText(name2);
+            email.setText(email2);
+            Glide.with(this).load(image_url).into(profilePic);
+            updateUI(true);
+        } else {
+
+            updateUI(false);
+        }
+    }
+
+    private void updateUI(boolean isLogin){
+
+        if(isLogin){
+
+            profileSection.setVisibility(View.VISIBLE);
+            signInButton.setVisibility(View.GONE);
+            logoImage.setVisibility(View.GONE);
+            noLogin.setVisibility(View.GONE);
+
+        } else {
+
+            profileSection.setVisibility(View.GONE);
+            signInButton.setVisibility(View.VISIBLE);
+            logoImage.setVisibility(View.VISIBLE);
+            noLogin.setVisibility(View.VISIBLE);
+        }
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode == REQ_CODE){
+
+            GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
+            handleResult(result);
+
+        }
     }
 }
