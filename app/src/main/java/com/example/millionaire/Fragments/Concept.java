568a172a57da26505ba5150681929843d4e58a36
Diogo Sequeira <diogosequeira@hotmail.fr>
2020-10-21 10:56:18 +0100

    Cleaned code, indentation.

--- app/src/main/java/com/example/millionaire/Fragments/Concept.java
+++ app/src/main/java/com/example/millionaire/Fragments/Concept.java
@@ -1,19 +1,13 @@
 package com.example.millionaire.Fragments;
-
 import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
-
 import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
-
 import com.example.millionaire.R;
-
-
 
 public class Concept extends Fragment {
 
@@ -22,20 +16,9 @@ public class Concept extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View v = inflater.inflate(R.layout.fragment_concept, container, false);
-
         TextView name = v.findViewById(R.id.name);
-
-        //TextView description = v.findViewById(R.id.);
-
-        //  String description = getArguments().getString("Descricao");
-        // description.setText(textDescri);
-
         String textName = getArguments().getString("Titulo");
         name.setText(textName);
-
-
         return v;
     }
-
 }
-
